122fe5eb589a6e0514384324f5c1b37fb99ee45c
alceu1 <alceu.junior@alunos.sc.senac.br>
2024-04-19 19:58:13 -0300

    Firts commit

---
+++ src/components/HamburguerAberto/Aberto.js
@@ -0,0 +1,9 @@
+import aberto from './Aberto.svg';
+const Aberto = () => {
+    return (
+        <div className='container-logo'>
+        <img src={aberto} alt='logo-marca'></img>
+      </div>
+    );
+}
+export default Aberto;
---
+++ src/components/menuBar/MenuBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './MenuBar.css';
+import Aberto from '../HamburguerAberto/Aberto';
+const MenuBar = () => {
+    return (
+        <div className="menu-bar">
+            <Aberto />
+        </div>
+    );
+}
+export default MenuBar;
